a9b79fa32d96eb16f2be780b1aed7deaf4480d35
Naman Aggarwal <naman.aggarwal2001@gmail.com>
2020-07-21 23:02:14 +0530

    Updates

---
+++ precache-manifest.77bd051fea4f331cc07d2e1b1668a2eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f931076532177b0a19765f159c1b7893",
+    "url": "/BRomine-BArium/index.html"
+  },
+  {
+    "revision": "e48dff105d5e0a297cf5",
+    "url": "/BRomine-BArium/static/css/main.9a7fe5c6.chunk.css"
+  },
+  {
+    "revision": "f36edc79c47bb1593fee",
+    "url": "/BRomine-BArium/static/js/2.3958c39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BRomine-BArium/static/js/2.3958c39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48dff105d5e0a297cf5",
+    "url": "/BRomine-BArium/static/js/main.da883431.chunk.js"
+  },
+  {
+    "revision": "f18e19e16b3cacd13b10",
+    "url": "/BRomine-BArium/static/js/runtime-main.30a9b732.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/BRomine-BArium/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/BRomine-BArium/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/BRomine-BArium/static/media/spinner.235b9b3e.gif"
+  }
+]);
